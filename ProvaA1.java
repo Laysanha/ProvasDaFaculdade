2b2f6e1337ab0539f6ae219e59904439a4c0eaa5
Laysa Santiago <55203249+Laysanha@users.noreply.github.com>
2021-10-25 19:17:09 -0300

    Add files via upload

---
+++ ProvaA1.java
@@ -0,0 +1,18 @@
+//package a1_primeiro_prova;
+//
+//import javax.swing.JOptionPane;
+//
+//public class ProvaA1 {
+//	public static void main(String[] args) {
+//		Double internetVel = Double
+//				.parseDouble(JOptionPane.showInputDialog(null, "Informe a velocidade da sua internet:"));
+//		Double file = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o tamanho do arquivo: "));
+//
+//		Double tempo = file / internetVel;
+//
+//		int minuto = (int) (tempo / 60);
+//		int seg = (int) (tempo % 60);
+//
+//		JOptionPane.showMessageDialog(null, "Seu download levará " + minuto + " minutos e " + seg + " segundos");
+//	}
+//}
